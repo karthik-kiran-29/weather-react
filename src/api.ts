4454dbe7a3287e007473b1913b284e473726cdcb
karthik-kiran-29 <karthikkiran029@gmail.com>
2026-02-25 10:56:22 +0530

    configration setup updated

--- src/api.ts
+++ src/api.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 
 const api =  axios.create({
   baseURL: 'https://weather.indianapi.in',
-  headers: { 'X-Api-Key': 'sk-live-MqSfVdzG8yXii6t2evey71KoZSmzlVXJ8xaCrSgn' }
+  headers: { 'X-Api-Key': import.meta.env.WEATHER_API }
 });
 
 export async function getCities(){
     return await api.get('/india/cities');
+}
+
+export async function getCityWeather(city_id:String){
+    return await api.get('/india/weather_by_id', { params: { city_id: city_id } })
 }
